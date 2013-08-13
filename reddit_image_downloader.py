c67ca8c0c4eaa46258edcd61e6b9a77008cbf4bd
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-08-13 17:50:59 +0200

    Add the basic functionality

---
+++ reddit_image_downloader.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import sys
+import traceback
+
+import praw
+import pyimgur
+import requests
+
+from authentication import IMGUR_CLIENT_ID, REDDIT_USERAGENT
+
+
+def main():
+    im = connect_to_imgur(IMGUR_CLIENT_ID)
+    r = connect_to_reddit(REDDIT_USERAGENT)
+    subreddit = ['cats', 'funny', 'pics']
+    submissions = get_submissions(r, subreddit)
+    new_images = []
+    for submission in submissions:
+        try:
+            name = sanitize_filename_windows(submission.title)
+            if is_direct_image_link(submission.url):
+                ext = submission.url.rpartition('.')[-1].lower()
+                name += "." + ext
+                with open(name, 'wb') as outfile:
+                    binary_image = requests.get(submission.url).content
+                    outfile.write(binary_image)
+                new_images.append(name)
+            elif im.is_imgur_url(submission.url):
+                just_downloaded = imgur_download(im, name, submission.url)
+                for image in just_downloaded:
+                    new_images.append(image)
+        except Exception:
+            print "-------------Error------------------"
+            traceback.print_exc()
+            sys.stderr.write("Link: " + submission.short_link + "\n")
+            sys.stderr.write("Url: " + submission.url + "\n")
+            sys.stderr.write("Subreddit: " + str(submission.subreddit) + "\n")
+            print "------------------------------------"
+    print "\n".join(new_images)
+
+
+def connect_to_imgur(client_id):
+    """Return a connected Imgur session."""
+    return pyimgur.Imgur(client_id)
+
+
+def connect_to_reddit(useragent):
+    """Return a connected Reddit session."""
+    return praw.Reddit(useragent)
+
+
+def is_direct_image_link(url):
+    """Return whether the url a direct link to an image."""
+    image_extensions = ('jpg', 'jpeg', 'png', 'gif', 'apng', 'tiff', 'bmp')
+    return url.rpartition('.')[-1].lower() in image_extensions
+
+
+def imgur_download(imgur_session, name, url):
+    """Download the Imgur image located at url and save it as name.
+
+    If it's an album subsequent images will have an underscore and a sequential
+    number as a suffix."""
+    obj = imgur_session.get_at_url(url)
+    images = obj.images if isinstance(obj, pyimgur.Album) else [obj]
+    results = []
+    for index, image in enumerate(images, 1):
+        new_name = name if len(images) == 1 else name + "_{}".format(index)
+        results.append(image.download(name=new_name))
+    return results
+
+
+def sanitize_filename_windows(name):
+    """Turn the filename into a legal filename.
+
+    See http://support.grouplogic.com/?p=1607
+    """
+    name = name.decode('utf-8')
+    name = name.encode('ascii', 'ignore')
+    name = name.replace('"', "'")
+    name = name.replace(" ", "_")  # Personal preference
+    for ch in "^/?<>\:*|”":
+        name = name.replace(ch, "")
+    return name[:255]
+
+
+def get_submissions(reddit_session, subreddits, listing='new', limit=3):
+    """Return the limit submissions made to subreddits' listing."""
+    multi_reddit_name = "+".join(subreddits)
+    multi_reddit = reddit_session.get_subreddit(multi_reddit_name)
+    # TODO. See if there is a way to optimize this. Looks repetitive
+    listings_methods = {'controversial': multi_reddit.get_controversial,
+                        'hot': multi_reddit.get_hot,
+                        'new': multi_reddit.get_new,
+                        'rising': multi_reddit.get_rising,
+                        'top': multi_reddit.get_top}
+    method = listings_methods.get(listing, multi_reddit.get_new)
+    return list(method(limit=limit))
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_reddit_image_downloader.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Should be used instead of Exception in test_get_submissions_private_sub and
+# test_get_submissions_nonexisting_sub. But weirdly enough it doesn't catch the
+# error even though it should be the same.
+# from urllib2 import HTTPError
+
+import os
+
+import pytest
+
+from authentication import IMGUR_CLIENT_ID, REDDIT_USERAGENT
+from reddit_image_downloader import *
+
+
+def test_connect_to_imgur():
+    im = connect_to_imgur(IMGUR_CLIENT_ID)
+    # Test the imgur session is functional
+    image = im.get_image('S1jmapR')
+    assert image.views > 1
+
+
+def test_connect_to_imgur_bad_argument():
+    # PyImgur uses lazy objects. The imgur session will not be validated until
+    # the first request has been made
+    im = connect_to_imgur("BAD_CLIENT_ID")
+    with pytest.raises(Exception):  # 403. Access Denied.
+        image = im.get_image('S1jmapR')
+        assert image.views > 1
+
+
+def test_connect_to_reddit():
+    r = connect_to_reddit(REDDIT_USERAGENT)
+    sub = r.get_submission(submission_id='16m0uu')
+    assert sub.author.name == 'bboe'
+
+
+def test_get_submissions():
+    subreddits = ['pics', 'cats', 'gifs', 'funny']
+    reddit_session = connect_to_reddit(REDDIT_USERAGENT)
+    posts = get_submissions(reddit_session, subreddits)
+    assert posts
+
+
+def test_get_submissions_private_sub():
+    subreddits = ['lounge']
+    reddit_session = connect_to_reddit(REDDIT_USERAGENT)
+    with pytest.raises(Exception):  # 403. Access Denied.
+        posts = get_submissions(reddit_session, subreddits)
+        assert not posts
+
+
+def test_get_submissions_nonexisting_sub():
+    subreddits = ['notexisting']
+    reddit_session = connect_to_reddit(REDDIT_USERAGENT)
+    with pytest.raises(Exception):  # 404. Access Denied.
+        posts = get_submissions(reddit_session, subreddits)
+        assert not posts
+
+
+def test_is_direct_url():
+    assert is_direct_image_link('http://www.imgur.com/n0D2J3C.jpeg')
+    assert is_direct_image_link('http://d24w6bsrhbeh9d.cloudfront.net/'
+                                'photo/aBK11LA_700b.jpg')  # 9gag
+    assert is_direct_image_link('http://www.paradoxplaza.com/sites/all/'
+                                'themes/paradoxplaza/logo.png')
+    assert is_direct_image_link('http://dota2gifs.com/gifs/EnigmaTI3.gif')
+    assert not is_direct_image_link('http://www.reddit.com')
+    assert not is_direct_image_link('ftp://http://www.reddit.com')
+    assert not is_direct_image_link('')
+
+
+def test_sanitize_file_name_illegal_chars():
+    assert sanitize_filename_windows("HelloWorld?.jpg") == "HelloWorld.jpg"
+    assert sanitize_filename_windows("HelloWorld.jpg?") == "HelloWorld.jpg"
+    assert sanitize_filename_windows('Hello"World".jpg') == "Hello'World'.jpg"
+
+
+def test_sanitize_filename_windows_file_name_unicode():
+    assert sanitize_filename_windows("HelloWøøørld.jpg?") == "HelloWrld.jpg"
+
+
+def test_sanitize_filename_windows_file_name_more_readable():
+    assert sanitize_filename_windows("Hello World.jpg") == "Hello_World.jpg"
+
+
+# TODO add proper setup and teardown functions
+def test_imgur_download():
+    im = connect_to_imgur(IMGUR_CLIENT_ID)
+    try:
+        name = 'test_download'
+        url = 'http://imgur.com/gallery/7RsFUmL'
+        result = imgur_download(im, name, url)
+        assert len(result) == 1
+        assert result[0] == name + ".jpg"
+    finally:
+        if 'result' in locals():
+            for file in result:
+                os.remove(file)
+
+
+def test_imgur_download_album():
+    im = connect_to_imgur(IMGUR_CLIENT_ID)
+    try:
+        name = 'test_download'
+        url = 'http://imgur.com/a/9kgGe'
+        result = imgur_download(im, name, url)
+        assert len(result) == 3
+        assert result[0] == name + "_1.jpg"
+        assert result[1] == name + "_2.jpg"
+        assert result[2] == name + "_3.jpg"
+    finally:
+        if 'result' in locals():
+            for file in result:
+                os.remove(file)
